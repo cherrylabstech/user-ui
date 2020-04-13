29950777ee1aff077395cc43e7327a14e2534735
gokulnath729 <gokulnath729@gmail.com>
2020-04-13 14:15:05 +0530

    pagintion

--- src/ReduxContainers/MyRequest.js
+++ src/ReduxContainers/MyRequest.js
@@ -1,13 +1,15 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Fragment, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { userActions } from "../ApiCall/rootApi";
 import { withRouter } from "react-router-dom";
-import queryString from "query-string";
+//import queryString from "query-string";
 function MyRequest(props) {
-  const query = queryString.parse(props.location.search);
-  const value = useSelector(state => state);
-  console.log(value);
-  const [page, setPage] = useState(parseInt(query.page) || 1);
+  //   const query = queryString.parse(props.location.search);
+  //   const ticketList = useSelector(state => state.TicketListReducer.TicketList);
+  //   const ticketListLoading = useSelector(
+  //     state => state.TicketListReducer.loading
+  //   );
+  //const [page, setPage] = useState(parseInt(query.page) || 1);
   const dispatch = useDispatch();
   useEffect(() => {
     const apiCall = () => {
@@ -15,26 +17,39 @@ function MyRequest(props) {
     };
     apiCall();
   }, [dispatch]);
-
+  //Ticket List Refresh
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     dispatch(userActions.TicketListRefreshApi(props.location.search));
+  //   }, 10000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // });
   //Ticket List
   useEffect(() => {
     dispatch(userActions.TicketListApi(props.location.search));
   }, [props.location.search, dispatch]);
-  let params = new URLSearchParams(props.location.search);
-  const handleIncrement = () => {
-    setPage(page + 1);
-    params.set("page", page + 1);
-    props.history.push({
-      pathname: "/request",
-      search: params.toString()
-    });
-  };
+  //let params = new URLSearchParams(props.location.search);
+  // const handleIncrement = () => {
+  //   setPage(page + 1);
+  //   params.set("page", page + 1);
+  //   props.history.push({
+  //     pathname: "/request",
+  //     search: params.toString()
+  //   });
+  // };
+  // const handleDecrement = () => {
+  //   setPage(page - 1);
+  //   params.set("page", page - 1);
+  //   props.history.push({
+  //     pathname: "/request",
+  //     search: params.toString()
+  //   });
+  // };
   return (
     <Fragment>
-      <div className="main">
-        My Request
-        <button onClick={handleIncrement}>Increment</button>
-      </div>
+      <div className="main">My Request</div>
     </Fragment>
   );
 }
