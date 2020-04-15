a6f4f9655b600c6e2428f46da77adf558c7ff88b
gokulnath729 <gokulnath729@gmail.com>
2020-04-15 19:08:55 +0530

    api and file upload

--- src/ReduxContainers/CreateTicket.js
+++ src/ReduxContainers/CreateTicket.js
@@ -14,14 +14,14 @@ import { userActions } from "../ApiCall/rootApi";
 const today = new Date(Date.now());
 
 function CreateTicket(props) {
-  const [name,setName] = useState()
+  const [name, setName] = useState();
   const [subject, setSubject] = useState();
   const [email, setEmail] = useState();
   const [radio, setRadio] = useState();
   const [dropValue, setDropValue] = useState();
   const [selectedDate, setSelectedDate] = useState(today);
-  const [assetCategory,setAssetCategory] =useState();
-  const [assetType,setAssetType] =useState();
+  const [assetCategory, setAssetCategory] = useState();
+  const [assetType, setAssetType] = useState();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -35,16 +35,16 @@ function CreateTicket(props) {
     // dispatch(userActions.LocationApi());
     // dispatch(userActions.UserApi());
     // dispatch(userActions.CompanyApi());
-     dispatch(userActions.AssetCategoryApi());
-     dispatch(userActions.ChooseAssetApi());
-     dispatch(userActions.AssetTypeApi());
+    // dispatch(userActions.AssetCategoryApi());
+    // dispatch(userActions.ChooseAssetApi());
+    // dispatch(userActions.AssetTypeApi());
 
     apiCall();
   }, [dispatch]);
 
-const handleName = e =>{
-  setName(e.target.value)
-}
+  const handleName = e => {
+    setName(e.target.value);
+  };
   const handleSubject = model => {
     setSubject(model);
   };
@@ -69,164 +69,228 @@ const handleName = e =>{
   const handleDate = date => {
     setSelectedDate(date);
   };
-  const handleAssetCategory = e =>{
-    setAssetCategory(e.target.value)
-  }
-const handelAssetType = e=>{
-setAssetType(e.target.value)
-}
-    const CreateFormData = useSelector(state => state.createTicketReducer.createTicketData);
+  const handleAssetCategory = e => {
+    setAssetCategory(e.target.value);
+  };
+  const handelAssetType = e => {
+    setAssetType(e.target.value);
+  };
+  const CreateFormData = useSelector(
+    state => state.createTicketReducer.createTicketData
+  );
   //  CATEGORY DROPDOWN LIST
-    const CategoryOptions = useSelector(state => state.requestCategoryReducer.requestCategoryData)
-    const CategoryList = (CategoryOptions||[]).map(data=>{
-      return <option value={data.id}>{data.category}</option>
-    })
-    // ASSET CATEGORY DROPDOWN LIST
-    const AssetCategoryOptions= useSelector(state => state.AssetCategoryReducer.AssetCategoryData)
-    const AssetCategoryList = (AssetCategoryOptions||[]).map(data=>{
-      return <option value={data.id}>{data.categoryName}</option>
-    })
-    // ASSET TYPE DROPDOWN LIST
-    const AssetTypeOptions= useSelector(state => state.AssetTypeReducer.AssetTypeData)
-    const AssetTypeList = (AssetTypeOptions||[]).map(data=>{
-      return <option value={data.id}>{data.model}</option>
-    })
-    // ASSET DROPDOWN LIST
-// const AssetOptions= useSelector(state => state.ChooseAssetReducer.ChooseAssetData)
+  const CategoryOptions = useSelector(
+    state => state.requestCategoryReducer.requestCategoryData
+  );
+  const CategoryList = (CategoryOptions || []).map(data => {
+    return <option value={data.id}>{data.category}</option>;
+  });
+  // ASSET CATEGORY DROPDOWN LIST
+  const AssetCategoryOptions = useSelector(
+    state => state.AssetCategoryReducer.AssetCategoryData
+  );
+  const AssetCategoryList = (AssetCategoryOptions || []).map(data => {
+    return <option value={data.id}>{data.categoryName}</option>;
+  });
+  // ASSET TYPE DROPDOWN LIST
+  const AssetTypeOptions = useSelector(
+    state => state.AssetTypeReducer.AssetTypeData
+  );
+  const AssetTypeList = (AssetTypeOptions || []).map(data => {
+    return <option value={data.id}>{data.model}</option>;
+  });
+  // ASSET DROPDOWN LIST
+  // const AssetOptions= useSelector(state => state.ChooseAssetReducer.ChooseAssetData)
 
-//  const AssetList = (AssetOptions.payload||[]).map(data=>{
-//       return <option value={data.id}>{data.model}</option>
-//     })
-    const AssetGroup = (CreateFormData||[]).map(element=>{
-  if(element.field_type_id === 101){
-    return <DropDown value={assetCategory} onChange={handleAssetCategory} text={element.field_label} placeholder={element.field_placeholder} options={AssetCategoryList} />
-  } else if(element.field_type_id === 107){
-    return <DropDown value={assetType} onChange={handelAssetType} text={element.field_label} placeholder={element.field_placeholder} options={AssetTypeList} />}
-   else if(element.field_type_id === 4){
-    return <DropDown value={dropValue} onChange={handleDrop} text={element.field_label} placeholder={element.field_placeholder}  />
-  }
-  return true
-})
+  //  const AssetList = (AssetOptions.payload||[]).map(data=>{
+  //       return <option value={data.id}>{data.model}</option>
+  //     })
+  const AssetGroup = (CreateFormData || []).map(element => {
+    if (element.field_type_id === 101) {
+      return (
+        <DropDown
+          value={assetCategory}
+          onChange={handleAssetCategory}
+          text={element.field_label}
+          placeholder={element.field_placeholder}
+          options={AssetCategoryList}
+        />
+      );
+    } else if (element.field_type_id === 107) {
+      return (
+        <DropDown
+          value={assetType}
+          onChange={handelAssetType}
+          text={element.field_label}
+          placeholder={element.field_placeholder}
+          options={AssetTypeList}
+        />
+      );
+    } else if (element.field_type_id === 4) {
+      return (
+        <DropDown
+          value={dropValue}
+          onChange={handleDrop}
+          text={element.field_label}
+          placeholder={element.field_placeholder}
+        />
+      );
+    }
+    return true;
+  });
 
-const form = (CreateFormData || []).map((element,i) => {
-  if (element.field_type_id === 80 ) {
-        return (<div className="create-ticket-field-cont">
-            <TextField
-              type="text"
-              placeholder={element.field_placeholder}
-              text={element.field_label}
-              required={true}
-              onChange={handleName}
-            />
-          </div> );
-      }
-  if (element.field_type_id === 1 || element.field_type_id === 82) {
-        return (<div className="create-ticket-field-cont">
-            <TextField
-              type="text"
-              placeholder={element.field_placeholder}
-              text={element.field_label}
-              required={true}
-              onChange={handleSubject}
-            />
-          </div> );
-      } else if (element.field_type_id === 2 ) {
-        return (<div className="create-ticket-field-cont">
-            {/* <TextArea
+  const form = (CreateFormData || []).map((element, i) => {
+    if (element.field_type_id === 80) {
+      return (
+        <div className="create-ticket-field-cont">
+          <TextField
+            type="text"
+            placeholder={element.field_placeholder}
+            text={element.field_label}
+            required={true}
+            onChange={handleName}
+          />
+        </div>
+      );
+    }
+    if (element.field_type_id === 1 || element.field_type_id === 82) {
+      return (
+        <div className="create-ticket-field-cont">
+          <TextField
+            type="text"
+            placeholder={element.field_placeholder}
+            text={element.field_label}
+            required={true}
+            onChange={handleSubject}
+          />
+        </div>
+      );
+    } else if (element.field_type_id === 2) {
+      return (
+        <div className="create-ticket-field-cont">
+          {/* <TextArea
               placeholder="Textarea"
               text="Description"
               required={true}
               onChange={handleSubject}
             /> */}
-            <div className="f-14">
-        <label>Description</label>
-      </div>
-      <div style={{marginTop:"10px"}}>
+          <div className="f-14">
+            <label>Description</label>
+          </div>
+          <div style={{ marginTop: "10px" }}>
             <FroalasEditor
               tag="textarea"
               model={subject}
               onModelChange={handleSubject}
               name={element.field_label}
             />
-            </div>
-          </div>);
-      } else if (element.field_type_id === 16 || element.field_type_id === 84 ){
-return <div className="create-ticket-field-cont">
-            <DropDown value={dropValue} onChange={handleDrop} text={element.field_label} placeholder={element.field_placeholder} options={CategoryList}  />
           </div>
-      } else if ( element.field_type === "dropdown" ){
-return <div className="create-ticket-field-cont">
-            <DropDown value={dropValue} onChange={handleDrop} text={element.field_label} placeholder={element.field_placeholder} />
-          </div>
-      } else if (element.field_type_id === 81){
-return  <div className="create-ticket-field-cont">
-            <TextField
-              type="email"
-              placeholder={element.field_placeholder}
-              text={element.field_label}
-              required={true}
-              value={email}
-              onChange={handleMail}
+        </div>
+      );
+    } else if (element.field_type_id === 16 || element.field_type_id === 84) {
+      return (
+        <div className="create-ticket-field-cont">
+          <DropDown
+            value={dropValue}
+            onChange={handleDrop}
+            text={element.field_label}
+            placeholder={element.field_placeholder}
+            options={CategoryList}
+          />
+        </div>
+      );
+    } else if (element.field_type === "dropdown") {
+      return (
+        <div className="create-ticket-field-cont">
+          <DropDown
+            value={dropValue}
+            onChange={handleDrop}
+            text={element.field_label}
+            placeholder={element.field_placeholder}
+          />
+        </div>
+      );
+    } else if (element.field_type_id === 81) {
+      return (
+        <div className="create-ticket-field-cont">
+          <TextField
+            type="email"
+            placeholder={element.field_placeholder}
+            text={element.field_label}
+            required={true}
+            value={email}
+            onChange={handleMail}
+          />
+        </div>
+      );
+    } else if (element.field_type === "email") {
+      return (
+        <div className="create-ticket-field-cont">
+          <TextField
+            type="email"
+            placeholder={element.field_placeholder}
+            text={element.field_label}
+            required={true}
+            value={email}
+            onChange={handleMail}
+          />
+        </div>
+      );
+    } else if (element.field_type === "radio") {
+      const list = element.field_options.map(data => {
+        return (
+          <div style={{ margin: "10px 0" }}>
+            <Radio
+              text={data.option_title}
+              value={data.option_title}
+              onChange={handleRadio}
             />
           </div>
-      }
-      else if (element.field_type === "email"){
-return  <div className="create-ticket-field-cont">
-            <TextField
-              type="email"
-              placeholder={element.field_placeholder}
-              text={element.field_label}
-              required={true}
-              value={email}
-              onChange={handleMail}
-            />
+        );
+      });
+      return (
+        <div className="create-ticket-field-cont">
+          <div className="f-14">
+            <label>{element.field_label}</label>
           </div>
-      }else if (element.field_type === "radio"){
-        const list = element.field_options.map(data => {
-          return (
-            
-            <div style={{margin:"10px 0"}}>
-              <Radio
-                text={data.option_title}
-                value={data.option_title}
-                onChange={handleRadio}
-              />
-            </div>
-          );
-        });
-return <div className="create-ticket-field-cont"><div className="f-14"><label>{element.field_label}</label></div>{list}</div>}
-else if(element.field_type === "checkbox"){
-  return <div className="create-ticket-field-cont">
-              <CheckBox
-                text={element.field_label}
-                value={element.field_label}
-                onChange={handleCheck}
-              />
-              </div>
-} else if(element.field_type === "date" ){
-return <div className="create-ticket-field-cont">
-<div className="f-14">
-        <label>{element.field_label}</label>
-      </div> 
-      <div style={{margin:"10px 0"}}>
+          {list}
+        </div>
+      );
+    } else if (element.field_type === "checkbox") {
+      return (
+        <div className="create-ticket-field-cont">
+          <CheckBox
+            text={element.field_label}
+            value={element.field_label}
+            onChange={handleCheck}
+          />
+        </div>
+      );
+    } else if (element.field_type === "date") {
+      return (
+        <div className="create-ticket-field-cont">
+          <div className="f-14">
+            <label>{element.field_label}</label>
+          </div>
+          <div style={{ margin: "10px 0" }}>
             <DatePicker
               selected={selectedDate}
               onChange={handleDate}
-            ></DatePicker></div>
+            ></DatePicker>
           </div>
-} else if(element.field_type_id === 4){
-  return <div>{AssetGroup}</div>
-}
-      return true
-})
-
-
+        </div>
+      );
+    } else if (element.field_type_id === 4) {
+      return <div>{AssetGroup}</div>;
+    }
+    return true;
+  });
 
   return (
     <Fragment>
       <div className=" main main-create-ticket">
         Create Ticket
-        <form onSubmit={handleSubmit} style={{width:"100%"}}>
+        <form onSubmit={handleSubmit} style={{ width: "100%" }}>
           {form}
           <div>
             <Button text="Submit" className="primary-btn btn-wide" />
