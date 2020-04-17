e62ea612ea36ae3f32d02668bfb4d6ebae8c3c34
gokuls2302 <gokuls2302@gmail.com>
2020-04-17 15:32:35 +0530

    ticket Detail

--- src/ReduxContainers/MyAssetDetail.js
+++ src/ReduxContainers/MyAssetDetail.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../ApiCall/rootApi";
 import { withRouter } from "react-router-dom";
+import { Timestamp } from "../helpers/Timestamp";
 import Spinner from "../ReusableComps/Spinner";
 function MyAssetDetail(props) {
   const dispatch = useDispatch();
@@ -16,17 +17,72 @@ function MyAssetDetail(props) {
   useEffect(() => {
     dispatch(userActions.AssetDetailApi(params.id));
   }, [dispatch]);
+
+// const extraData = assetDetailData.customFields;
+    // const detailData = (extraData || []).map((data, index) => {
+    //   return (
+    //     <tr key={index}>
+    //       <td>{data.name}</td>
+    //       <td>{data.data}</td>
+    //     </tr>
+    //   );
+    // });
+
   return (
     <Fragment>
       <div className="main">
         My Asset Detail
+        <div className="asset-detail-page">
         {assetDetailLoading ? (
           <div>
             <Spinner fontSize="30px"></Spinner>
           </div>
         ) : (
-          assetDetailData && <div>{assetDetailData.team}></div>
-        )}
+          <div className="asset-table">
+          {assetDetailData && <table>
+                    <tbody>
+                      <tr>
+                        <td>Asset Name</td>
+                        <td>{assetDetailData.item}</td>
+                      </tr>
+                      <tr>
+                        <td>Asset Descript</td>
+                        <td>{assetDetailData.itemDesc}</td>
+                      </tr>
+                      <tr>
+                        <td>Category Name</td>
+                        <td>{assetDetailData.category}</td>
+                      </tr>
+                      <tr>
+                        <td>Support Team</td>
+                        <td>{assetDetailData.team}</td>
+                      </tr>
+                      <tr>
+                        <td>Asset Type</td>
+                        <td>{assetDetailData.model}</td>
+                      </tr>
+                      <tr>
+                        <td>Asset State</td>
+                        <td>{assetDetailData.state}</td>
+                      </tr>
+                      <tr>
+                        <td>Start Date</td>
+                        <td>
+                          {assetDetailData.startDate === null ? null : Timestamp(assetDetailData.startDate)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>End Date</td>
+                        <td>
+                          {assetDetailData.endDate === 0 || assetDetailData.endDate === null
+                            ? null
+                            : Timestamp(assetDetailData.endDate)}
+                        </td>
+                      </tr>
+                      {/* {detailData} */}
+                    </tbody>
+                  </table>
+        }</div>)}</div>
       </div>
     </Fragment>
   );
