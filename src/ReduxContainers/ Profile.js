500ff65e2433fc1404de900dc326da81dbf5f27d
gokulnath729 <gokulnath729@gmail.com>
2020-04-17 14:01:09 +0530

    aset

--- src/ReduxContainers/ Profile.js
+++ src/ReduxContainers/ Profile.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../ApiCall/rootApi";
 import FileUpload from "../components/FileUpload/FileUpload";
 function Profile() {
   const dispatch = useDispatch();
+  const assetCount = useSelector(state => state);
+  console.log(assetCount);
   useEffect(() => {
     const token = localStorage.getItem("X-Auth-Token");
     const tokenApiCalls = () => {
@@ -13,10 +15,7 @@ function Profile() {
   }, [dispatch]);
   return (
     <Fragment>
-      <div className="main">
-        My Profile
-        <FileUpload></FileUpload>
-      </div>
+      <div className="main">My Profile</div>
     </Fragment>
   );
 }
