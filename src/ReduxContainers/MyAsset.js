306ce03d68f806eea9310523108b64ef5f8d7959
gokulnath729 <gokulnath729@gmail.com>
2020-04-17 11:04:34 +0530

    asset

--- src/ReduxContainers/MyAsset.js
+++ src/ReduxContainers/MyAsset.js
@@ -1,13 +1,15 @@
 import React, { Fragment, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../ApiCall/rootApi";
 import { withRouter } from "react-router-dom";
-import PopUp from "../components/PopUp/PopUp";
+import Spinner from "../ReusableComps/Spinner";
 //import queryString from "query-string";
 
 function MyAsset(props) {
   //const query = queryString.parse(props.location.search);
-  //const asset = useSelector(state => state.AssetListReducer.AssetList);
+  const assetData = useSelector(state => state.AssetListReducer.AssetList);
+  const assetLoading = useSelector(state => state.AssetListReducer.loading);
+  const assetCount = useSelector(state => state.AssetCountReducer.AssetCount);
   // let params = new URLSearchParams(props.location.search);
   // const [page, setPage] = useState(parseInt(query.page) || 1);
   const dispatch = useDispatch();
@@ -37,11 +39,26 @@ function MyAsset(props) {
   //     search: params.toString()
   //   });
   // };
+  const assetDatas = assetData !== undefined && assetData.payload;
   return (
     <Fragment>
       <div className="main">
         My Asset
-        <PopUp></PopUp>
+        <div>
+          asset count : {assetCount !== undefined && assetCount.assetCount}
+        </div>
+        <div>
+          {assetLoading ? (
+            <div>
+              <Spinner fontSize="40px"></Spinner>
+            </div>
+          ) : (
+            <div>
+              {assetDatas &&
+                assetDatas.map(data => <div key={data.id}>{data.item}</div>)}
+            </div>
+          )}
+        </div>
       </div>
     </Fragment>
   );
