addd1b25e9b98fc0f3cb2c17422ef71a96c1b62a
gokulnath729 <gokulnath729@gmail.com>
2020-04-20 16:21:43 +0530

    profile upload completed

--- src/ReduxContainers/ Profile.js
+++ src/ReduxContainers/ Profile.js
@@ -3,8 +3,10 @@ import Modal from "react-modal";
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import EditProfile from "./EditProfile";
+import EditPassword from "./EditPassword";
 function Profile(props) {
   const [isPaneOpen, setIsPaneOpen] = useState(false);
+  const [isPasswordPaneOpen, setIsPasswordPaneOpen] = useState(false);
   let el = React.createRef();
   useEffect(() => {
     Modal.setAppElement(el);
@@ -15,11 +17,18 @@ function Profile(props) {
   const openSlidePane = () => {
     setIsPaneOpen(true);
   };
+  const closeSlidingPasswordPane = () => {
+    setIsPasswordPaneOpen(false);
+  };
+  const openPasswordSlidePane = () => {
+    setIsPasswordPaneOpen(true);
+  };
   return (
     <Fragment>
       <div ref={ref => (el = ref)} className="main">
         Edit Profile
         <button onClick={openSlidePane}>Edit Profile</button>
+        <button onClick={openPasswordSlidePane}>Edit Password</button>
         <SlidingPane
           title="Edit Profile"
           overlayClassName="slide-overlay"
@@ -32,6 +41,15 @@ function Profile(props) {
             profilePicture={props.profilePicture}
             loading={props.loading}
           ></EditProfile>
+        </SlidingPane>
+        <SlidingPane
+          title="Edit Password"
+          overlayClassName="slide-overlay"
+          isOpen={isPasswordPaneOpen}
+          width="340px"
+          onRequestClose={closeSlidingPasswordPane}
+        >
+          <EditPassword></EditPassword>
         </SlidingPane>
       </div>
     </Fragment>
