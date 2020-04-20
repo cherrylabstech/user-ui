3f9e7aa5d19436c84adfb4ddeb89a0edc4e94380
gokulnath29 <gokulnath.r@asista.com>
2020-04-20 17:28:03 +0530

    EditPassword

--- src/ReduxContainers/EditPassword.js
+++ src/ReduxContainers/EditPassword.js
@@ -1,8 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import TextField from "../ReusableComps/TextField";
 function EditPassword() {
+  const [current,setCurrent]=useState()
+  const [newPassword,setNewPassword]=useState()
+  const [confPassword,setConfPassword]=useState()
+
+  const handleCurrent= (e)=>{
+setCurrent(e.target.value)
+  }
+  const handleNewPass= (e)=>{
+setNewPassword(e.target.value)
+  }
+  const handleConformPass= (e)=>{
+setConfPassword(e.target.value)
+  }
+
   return (
     <Fragment>
-      <div className="main">Edit Password</div>
+      <div className="my-10"><TextField text="Current Password" placeholder="Type Current Password" onChange={handleCurrent}/></div>
+      <div className="my-10"><TextField text="New Password" placeholder="Type New Password" onChange={handleNewPass}/></div>
+     <div className="my-10"><TextField text="Conform New Password" placeholder="Conform New Password" onChange={handleConformPass}/></div>
     </Fragment>
   );
 }
