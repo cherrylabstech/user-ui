b447b277958e8e44e5bde2b36a4c0290baf43ad6
gokuls23 <gokuls2302@gmail.com>
2020-06-26 13:36:43 +0530

    routes

--- src/ApiBasePath/ApiBasePath.js
+++ src/ApiBasePath/ApiBasePath.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "";
+export const BASE_PATH = "https://dev.asista.in";
 export const SERVICE_PATH = "/servicedesk/api/v1";
 export const SPARROW_PATH = "/SparrowdeskReports/api";
 // https://dev.asista.in
